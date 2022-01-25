95c163a238b11a14ee32dc34eb9a7d5325197c07
SWIPHY <83208039+SWIPHY@users.noreply.github.com>
2022-01-25 08:27:10 +0100

    Update Generator.py

--- Generator.py
+++ Generator.py
@@ -13,35 +13,33 @@ def toString(sudoku):
             sortie += str(sudoku[i][j])
     return sortie + "\n"
 
-def vérif_Validité(sudoku, row, collone):
+def vérif_Validité(sudoku, row, colonne):
     """
     Renvoie True si une cellule donnée est valide dans le Sudoku, sinon Faux. 
     Une case est valide si le nombre dans cette meme case n'est pas présente dans l'une des autres cases de la même ligne,
     de la même colonne ou du même bloc.
     """
     block_row = row // 3
-    block_collone = collone // 3
+    block_colonne = colonne // 3
 
-    # Row and Column
-    # Ignore blank spots
     for m in range(9):
-        if sudoku[row][m] != 0 and m != collone and sudoku[row][m] == sudoku[row][collone]:
+        if sudoku[row][m] != 0 and m != colonne and sudoku[row][m] == sudoku[row][colonne]:
             return False
-        if sudoku[m][collone] != 0 and m != row and sudoku[m][collone] == sudoku[row][collone]:
+        if sudoku[m][colonne] != 0 and m != row and sudoku[m][colonne] == sudoku[row][colonne]:
             return False
 
     # Block
     for m in range(3):
         for n in range(3):
             newRow = m + block_row*3
-            newCol = n + block_collone*3
-            if sudoku[newRow][newCol] != 0 and newRow != row and newCol != collone\
-            and sudoku[newRow][newCol ] == sudoku[row][collone]:
+            newCol = n + block_colonne*3
+            if sudoku[newRow][newCol] != 0 and newRow != row and newCol != colonne\
+            and sudoku[newRow][newCol ] == sudoku[row][colonne]:
                 return False
 
     return True
 
-def populateBoard(sudoku, row, collone):
+def populateBoard(sudoku, row, colonne):
     """
     À partir d'une grille 9x9 de 0, cette fonction remplit récursivement
     la grille. Il fait une liste d'entiers de 1 à 9, mélange l'ordre et
@@ -50,27 +48,27 @@ def populateBoard(sudoku, row, collone):
     il essaie le suivant dans la liste. Si aucun des nombres entiers ne fonctionne, alors
     il défini sur vide et retourne false.
     """
-    if row == 8 and collone == 8:
-        used = pySudoku.test_cell(sudoku, row, collone)
-        sudoku[row][collone] = used.index(0)
+    if row == 8 and colonne == 8:
+        used = pySudoku.test_cell(sudoku, row, colonne)
+        sudoku[row][colonne] = used.index(0)
         return True
 
-    if collone == 9:
+    if colonne == 9:
         row = row+1
-        collone = 0
+        colonne = 0
 
     temp = list(range(1, 10))
     random.shuffle(temp)
-    # Fill Sudoku
+    
     for i in range(9):
-        sudoku[row][collone] = temp[i]
-        if vérif_Validité(sudoku, row, collone):
-            if populateBoard(sudoku, row, collone+1):
+        sudoku[row][colonne] = temp[i]
+        if vérif_Validité(sudoku, row, colonne):
+            if populateBoard(sudoku, row, colonne+1):
                 return True
-    sudoku[row][collone] = 0
+    sudoku[row][colonne] = 0
     return False
 
-def DFS_solve(copy_s, row, collone):
+def DFS_solve(copy_s, row, colonne):
     """
     Résout récursivement la grille avec un retour en arrière un utilisant
     l'algorithme DFS, en retournant le nombre de solutions trouvées.
@@ -80,31 +78,31 @@ def DFS_solve(copy_s, row, collone):
     num_solutions = 0
 
     # Atteint les dernières cellules sans aucune erreur, il y a donc une solution
-    if row == 8 and collone == 8:
+    if row == 8 and colonne == 8:
         return num_solutions + 1
 
-    if collone == 9:
+    if colonne == 9:
         row = row+1
-        collone = 0
+        colonne = 0
 
-    if copy_s[row][collone] == 0:
+    if copy_s[row][colonne] == 0:
         # Used = liste de taille 10 représentant quels nombres sont possibles
         # Dans la grille: 0 = possible, 1 = impossible.
-        used = pySudoku.test_cell(copy_s, row, collone)
+        used = pySudoku.test_cell(copy_s, row, colonne)
         # Aucune solution possible. Renvoie 0 pour le nombre de solutions.
         if 0 not in used:
             return 0
 
         while 0 in used:
-            copy_s[row][collone] = used.index(0)
+            copy_s[row][colonne] = used.index(0)
             used[used.index(0)] = 1
-            num_solutions += DFS_solve(copy_s, row, collone+1)
+            num_solutions += DFS_solve(copy_s, row, colonne+1)
 
         # Reached here? Then we tried 1-9 without success
-        copy_s[row][collone] = 0
+        copy_s[row][colonne] = 0
         return num_solutions
 
-    num_solutions += DFS_solve(copy_s, row, collone+1)
+    num_solutions += DFS_solve(copy_s, row, colonne+1)
     return num_solutions
 
 def reduce_sudoku(sudoku, niveau_difficultée):
@@ -120,9 +118,9 @@ def reduce_sudoku(sudoku, niveau_difficultée):
 
     while indices:
         row = indices[0] // 9
-        collone = indices[0] % 9
-        temp = sudoku[row][collone]
-        sudoku[row][collone] = 0
+        colonne = indices[0] % 9
+        temp = sudoku[row][colonne]
+        sudoku[row][colonne] = 0
         indices = indices[1:]
 
         copy_s = [l[:] for l in sudoku]
@@ -134,11 +132,7 @@ def reduce_sudoku(sudoku, niveau_difficultée):
                 num_solutions = DFS_solve(copy_s, 0, 0)
                # Si la grille n'a pas de solution unique, alors ça annule la dernière insertion.
                 if num_solutions > 1:
-                    sudoku[row][collone] = temp
-                    # If we want easy puzzles, we would stop here after finding
-                    # the first puzzle with a unique solution without attempting to
-                    # make it harder by trying to remove other elements and seeing
-                    # if there is another harder puzzle with a unique solution
+                    sudoku[row][colonne] = temp
                     if niveau_difficultée == "E" or niveau_difficultée == "e":
                         return
                 break
@@ -158,7 +152,7 @@ def main():
     start = time.time()
 
     for _ in range(entrée_user):
-        # 9 x 9 grid of 0s
+        # 9 x 9
         sudoku = [[0]*9 for _ in range(9)]
 
         populateBoard(sudoku, 0, 0)
@@ -166,7 +160,7 @@ def main():
         sortie = toString(sudoku)
         f.write(sortie)
 
-    print("{:.2f} seconds to come up with {} Sudoku puzzles.".format(time.time() - start, entrée_user))
+    print("{:.2f} seconds pour créer {} grilles de Sudoku.".format(time.time() - start, entrée_user))
 
 if __name__ == '__main__':
     main()
